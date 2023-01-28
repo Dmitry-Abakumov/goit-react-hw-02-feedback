b6d251a956edea60c462b557596eceeec63a804e
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-01-28 15:51:05 +0200

    Создает виджет отзывов

---
+++ src/modules/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+import Box from 'shared/components/Box/Box';
+import Button from 'shared/components/Button/Button';
+
+const FeedbackOptions = ({ options, onFeedbackBtnClick }) => {
+  return (
+    <Box display="flex" gridGap={10} mb={10} as="ul">
+      {options.map(({ id, option }) => (
+        <li key={id}>
+          <Button option={option} onClick={onFeedbackBtnClick} type="button" />
+        </li>
+      ))}
+    </Box>
+  );
+};
+
+export default FeedbackOptions;
+
+FeedbackOptions.defaultProps = {
+  options: [],
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      option: PropTypes.string.isRequired,
+    })
+  ),
+  onFeedbackBtnClick: PropTypes.func.isRequired,
+};
